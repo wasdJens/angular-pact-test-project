400922645a65eb2c24066da1f07676d8c5587452
Jens Reiner <Jens.Reiner@plan-b-gmbh.com>
2021-08-05 15:22:07 +0200

    🚧 Re-enable test code

--- src/app/weather-service/weather.service.pact.spec.ts
+++ src/app/weather-service/weather.service.pact.spec.ts
@@ -2,10 +2,10 @@ import {TestBed} from '@angular/core/testing';
 import {HttpClientModule} from '@angular/common/http';
 import {WeatherService} from './weather.service';
 import {WeatherForecast} from './weatherforecast';
-// import {Pact, Matchers} from '@pact-foundation/pact';
+import {Pact, Matchers} from '@pact-foundation/pact';
 import { PactWeb } from '@pact-foundation/pact-web';
 // import { string } from '@pact-foundation/pact/src/dsl/matchers';
-const { Pact } = require("@pact-foundation/pact")
+// const { Pact } = require("@pact-foundation/pact")
 
 describe('WeatherService', () => {
 
@@ -55,29 +55,29 @@ describe('WeatherService', () => {
     const expectedForecasts: Array<WeatherForecast> = [];
 
     beforeAll((done) => {
-      // provider.addInteraction({
-      //   state: `the weather API returns the forecast for the next 5 days`,
-      //   uponReceiving: 'a request to get the forecast',
-      //   withRequest: {
-      //     method: 'GET',
-      //     path: '/weatherforecast',
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     }
-      //   },
-      //   willRespondWith: {
-      //     status: 200,
-      //     body: Matchers.eachLike({
-      //         date: Date,
-      //         temperatureC: Number,
-      //         temperatureF: Number,
-      //         summary: "",
-      //     },{min:5}),
-      //     headers: {
-      //       'Content-Type': 'application/json'
-      //     }
-      //   }
-      // }).then(done, error => done.fail(error));
+      provider.addInteraction({
+        state: `the weather API returns the forecast for the next 5 days`,
+        uponReceiving: 'a request to get the forecast',
+        withRequest: {
+          method: 'GET',
+          path: '/weatherforecast',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        },
+        willRespondWith: {
+          status: 200,
+          body: Matchers.eachLike({
+              date: Date,
+              temperatureC: Number,
+              temperatureF: Number,
+              summary: "",
+          },{min:5}),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      }).then(done, error => done.fail(error));
     });
 
     it('should get the next 5 days', (done) => {
